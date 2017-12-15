2407a8f465ecd96af57c21025c395a3f057d53dd
Sahil Bansal <sahil@Sahils-MBP.attlocal.net>
2017-12-14 16:01:57 -0800

    First Commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  var timeData = [],
+    temperatureData = [],
+    humidityData = [];
+  var data = {
+    labels: timeData,
+    datasets: [
+      {
+        fill: false,
+        label: 'Temperature',
+        yAxisID: 'Temperature',
+        borderColor: "rgba(255, 204, 0, 1)",
+        pointBoarderColor: "rgba(255, 204, 0, 1)",
+        backgroundColor: "rgba(255, 204, 0, 0.4)",
+        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+        data: temperatureData
+      }
+    ]
+  }
+
+  var basicOption = {
+    title: {
+      display: true,
+      text: 'Muscle Rep Real-time Data',
+      fontSize: 36
+    },
+    scales: {
+      yAxes: [{
+        id: 'Muscle Rep',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Muscle Rep(0 or 1)',
+          display: true
+        },
+        position: 'left',
+      }]
+    }
+  }
+
+  //Get the context of the canvas element we want to select
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var optionsNoAnimation = { animation: false }
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: basicOption
+  });
+
+  var ws = new WebSocket('wss://' + location.host);
+  ws.onopen = function () {
+    console.log('Successfully connect WebSocket');
+  }
+  ws.onmessage = function (message) {
+    console.log('receive message' + message.data);
+    try {
+      var obj = JSON.parse(message.data);
+      if(!obj.time || !obj.temperature) {
+        return;
+      }
+      timeData.push(obj.time);
+      temperatureData.push(obj.temperature);
+      // only keep no more than 50 points in the line chart
+      const maxLen = 50;
+      var len = timeData.length;
+      if (len > maxLen) {
+        timeData.shift();
+        temperatureData.shift();
+      }
+      myLineChart.update();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
