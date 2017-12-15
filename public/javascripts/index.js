589ae03cd1d50e5c457ef9fd797b85941b149425
Sahil Bansal <sahil@Sahils-MBP.attlocal.net>
2017-12-14 17:34:14 -0800

    Commit with Original Code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,6 +15,17 @@ $(document).ready(function () {
         pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
         pointHoverBorderColor: "rgba(255, 204, 0, 1)",
         data: temperatureData
+      },
+      {
+        fill: false,
+        label: 'Humidity',
+        yAxisID: 'Humidity',
+        borderColor: "rgba(24, 120, 240, 1)",
+        pointBoarderColor: "rgba(24, 120, 240, 1)",
+        backgroundColor: "rgba(24, 120, 240, 0.4)",
+        pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
+        pointHoverBorderColor: "rgba(24, 120, 240, 1)",
+        data: humidityData
       }
     ]
   }
@@ -22,19 +33,27 @@ $(document).ready(function () {
   var basicOption = {
     title: {
       display: true,
-      text: 'Muscle Rep Real-time Data',
+      text: 'Temperature & Humidity Real-time Data',
       fontSize: 36
     },
     scales: {
       yAxes: [{
-        id: 'Muscle Rep',
+        id: 'Temperature',
         type: 'linear',
         scaleLabel: {
-          labelString: 'Muscle Rep(0 or 1)',
+          labelString: 'Temperature(C)',
           display: true
         },
         position: 'left',
-      }]
+      }, {
+          id: 'Humidity',
+          type: 'linear',
+          scaleLabel: {
+            labelString: 'Humidity(%)',
+            display: true
+          },
+          position: 'right'
+        }]
     }
   }
 
@@ -67,6 +86,14 @@ $(document).ready(function () {
         timeData.shift();
         temperatureData.shift();
       }
+
+      if (obj.humidity) {
+        humidityData.push(obj.humidity);
+      }
+      if (humidityData.length > maxLen) {
+        humidityData.shift();
+      }
+
       myLineChart.update();
     } catch (err) {
       console.error(err);
